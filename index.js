b4ce4bb86ad4fda31a9aed1378d1674b744d6bf8
ConnorPorter-dev <52919851+ConnorPorter-dev@users.noreply.github.com>
2020-04-19 13:53:52 +0100

    id

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').load();
+const id = ">"
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+  if (msg.content === id +'ping') {
     msg.reply('Pong!');
   }
 });
